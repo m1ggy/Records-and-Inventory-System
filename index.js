5646624aeb26ec798d847665b849618eb5c27672
m1ggy <46958815+m1ggy@users.noreply.github.com>
2019-11-10 20:32:43 +0800

    finished the project

--- index.js
+++ index.js
@@ -8,26 +8,15 @@ const adapterInvent = new FileSync('db1.json')
 const db1 = low(adapterInvent);
 const adapterArchive = new FileSync('Archive.json');
 const dbArchive = low(adapterArchive);
+const shortid = require('shortid');
 let mainWindow;
 let secondWindow;
 let counter;
 let inventoryCounter;
+let menuTemplate;
+let menuLogin;
+let subMenu
 
-if(db.has('user').value == true){
-    counter = db.get('user').map(`count`).reverse().take(1).value();
- }
- else{
- counter = 0;
- }
-
-
-if(db1.has('Item').value == true){
-    inventoryCounter = db1.get('Item').map(`Count`).reverse().take(1).value();
- }
- else
- inventoryCounter = 0;
-
-   
 app.on('ready',()=>{
     mainWindow = new BrowserWindow({
         webPreferences: {
@@ -35,9 +24,12 @@ app.on('ready',()=>{
         }
        
     });
+    const menu = Menu.buildFromTemplate(menuLogin);
+    Menu.setApplicationMenu(menu);
     mainWindow.loadURL(`File://${__dirname}/index.html`);
-
     mainWindow.on('closed', () =>{
+        mainWindow = null;
+        app.quit();
         
     })
 
@@ -53,8 +45,12 @@ function addNewWindow(){
             nodeIntegration: true
             }
     });
+    const menu = Menu.buildFromTemplate(subMenu);
+    Menu.setApplicationMenu(menu);
     secondWindow.loadURL(`File://${__dirname}/add_records.html`);
     secondWindow.on('closed', ()=>{
+        const menu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(menu);
         secondWindow = null;
     });
 };
@@ -68,12 +64,35 @@ function addNewInventoryWindow(){
             nodeIntegration: true
             }
     });
+    const menu = Menu.buildFromTemplate(subMenu);
+    Menu.setApplicationMenu(menu);
     secondWindow.loadURL(`File://${__dirname}/add_inventory.html`);
     
     secondWindow.on('closed', ()=>{
+        const menu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(menu);
         secondWindow = null;
     });
 
+}
+function updateItemWindow(){
+    secondWindow = new BrowserWindow({
+        height:450,
+        width:800,
+        title:'Update Inventory',
+        webPreferences: {
+            nodeIntegration: true
+            }
+    })
+    const menu = Menu.buildFromTemplate(subMenu);
+    Menu.setApplicationMenu(menu);
+    secondWindow.loadURL(`File://${__dirname}/update_item_inventory.html`);
+    
+    secondWindow.on('closed', ()=>{
+        const menu = Menu.buildFromTemplate(menuTemplate);
+        Menu.setApplicationMenu(menu);
+        secondWindow = null;
+    });
 }
 
 
@@ -83,6 +102,14 @@ addNewWindow();
 });
 
 ipcMain.on('record:add',(e, employee, service, payment,dateOfToday,timeOfToday) =>{
+
+    if(db.has('user[0].count').value){
+        counter = db.get('user').map(`count`).reverse().take(1).value();
+     }
+     else{
+     counter = 0;
+     }
+
     counter++;
 
     mainWindow.webContents.send('records:add', employee, service, payment, counter,dateOfToday,timeOfToday);
@@ -94,8 +121,15 @@ ipcMain.on('open:addinventory',e=>{
 });
 
 ipcMain.on('inventory:addNew', (e, item, quantity, payment,dateOfToday,timeOfToday) =>{
+    if(db1.has('Item').value){
+        inventoryCounter = db1.get('Item').map(`Count`).reverse().take(1).value();
+     }
+     else{
+     inventoryCounter = 0;
+     }
     inventoryCounter++;
-    mainWindow.webContents.send('inventory:add', item, quantity, payment, dateOfToday,timeOfToday,inventoryCounter);
+    let itemID = shortid.generate();
+    mainWindow.webContents.send('inventory:add', item, quantity, payment, dateOfToday,timeOfToday,inventoryCounter,itemID);
     secondWindow.close();
 })
 
@@ -104,7 +138,10 @@ ipcMain.on('password:changed', () =>{
 })
 
 ipcMain.on('login:success',e=>{
+    const menu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(menu);
     mainWindow.loadURL(`File://${__dirname}/home.html`);
+    
 });
 ipcMain.on('open:archiverecords', e=>{
     mainWindow.loadURL(`File://${__dirname}/archive_records.html`);
@@ -116,8 +153,73 @@ ipcMain.on('archive:goback', ()=>{
 ipcMain.on('archive:records', e=>{
     mainWindow.loadURL(`File://${__dirname}/records.html`);
 });
+ipcMain.on('update:inventory',()=>{
+    updateItemWindow();
+})
+ipcMain.on('update:success',()=>{
+    secondWindow.close();
+    mainWindow.loadURL(`File://${__dirname}/inventory.html`);
+})
 
+menuTemplate=[
+{
+    label:'File',
+        submenu:[
+            {
+                label:'Quit',
+                click:()=>{
+                    mainWindow.close();
+                },
+                accelerator:'Ctrl+Q'
+            }
+        ]
+    },
+    {
+    label:'Records',
+        submenu:[
+            {
+                label:'Add new record',
+                click:()=>{
+                    addNewWindow();
+                },
+                accelerator:'Ctrl+N'
+            },
+            {
+                label:'View archived records',
+                click:()=>{
+                    mainWindow.loadURL(`File://${__dirname}/view_archive_records.html`);
+                },
+                accelerator:'Ctrl+R'
+            }
 
-
-
-
+        ]},{
+        label:'Inventory',
+        submenu:[
+            {
+                label:'Add new item',
+                click:()=>{
+                    addNewInventoryWindow();
+                },
+                accelerator:'Ctrl+Shift+N'
+            },
+            {
+                label:'Update item',
+                click:()=>{
+                    updateItemWindow();
+                },
+                accelerator:'Ctrl+Shift+R'
+            }
+        ]
+}
+]
+menuLogin=[
+    {
+        label:'Please Login first!',
+        
+    }
+]
+subMenu=[
+    {
+        label:' '
+    }
+]
